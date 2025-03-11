7b45b45cf21c0ab940ec00819fc9457aee4d0806
CESAR LUIS REYES LOPEZ <clreyesl@ultragroupla.com>
2025-03-11 16:10:57 -0500

    feature: add rabbitmq and massTransint on notification api

--- AirlineBookingSystem/AirlineBookingSystem.Notification.Application/Services/NotificationService.cs
+++ AirlineBookingSystem/AirlineBookingSystem.Notification.Application/Services/NotificationService.cs
@@ -1,13 +1,22 @@
 ﻿namespace AirlineBookingSystem.Notification.Application.Services;
 
+using AirlineBookingSystem.BuildingBlocks.Contracts.EventBusMessages;
 using AirlineBookingSystem.Notification.Application.Interfaces;
+using MassTransit;
 
-public class NotificationService : INotificationService
+public class NotificationService(IPublishEndpoint publishEndpoint) : INotificationService
 {
-    public Task SendNotificationAsync(Domain.Entities.Notification notification)
+    private readonly IPublishEndpoint publishEndpoint = publishEndpoint;
+
+    public async Task SendNotificationAsync(Domain.Entities.Notification notification)
     {
         Console.WriteLine("datos enviandos");
 
-        return Task.CompletedTask;
+        //publish notification
+        var notificationEvent = new NotificationEvent(notification?.Recipient ?? string.Empty,
+                                                      notification?.Message ?? string.Empty,
+                                                      notification?.Type.ToString() ?? string.Empty);
+
+        await this.publishEndpoint.Publish(notificationEvent);
     }
 }
